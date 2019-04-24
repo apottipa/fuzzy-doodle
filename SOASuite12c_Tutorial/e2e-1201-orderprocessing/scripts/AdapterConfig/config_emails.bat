0357bcdfd7becc71c697cdb2ad92ae8ef09659d5
apottipa <pottipati.anudeep@gmail.com>
2019-04-23 20:06:09 -0400

    Added New Files

---
+++ SOASuite12c_Tutorial/e2e-1201-orderprocessing/scripts/AdapterConfig/config_emails.bat
@@ -0,0 +1,2 @@
+copy usermessagingconfig.xml C:\MW_BPM12C\user_projects\domains\Soa12CDomain\config\fmwconfig
+echo Email Configuration has been updated. Please restart the server for changes to take effect.
